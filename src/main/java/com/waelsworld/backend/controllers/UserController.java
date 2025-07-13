013bdfe7815e1c5b2ddda07d45e1c423ccc7a5b8
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-12 22:10:14 -0400

    added user controller for user related routes

--- src/main/java/com/waelsworld/backend/controllers/UserController.java
+++ src/main/java/com/waelsworld/backend/controllers/UserController.java
@@ -1,15 +1,28 @@
 package com.waelsworld.backend.controllers;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.waelsworld.backend.models.User;
+import com.waelsworld.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
-    @GetMapping
-    public String index() {
-        return "Welcome to Bike Garage!";
+    private final UserRepository userRepo;
+
+    @PostMapping
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        user.setId(null); // ensure new user
+        return ResponseEntity.ok(userRepo.save(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userRepo.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
