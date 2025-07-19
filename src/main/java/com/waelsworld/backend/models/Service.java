51fb9151648a4dd8f9aafe32281c1f1a0430ae1e
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-18 22:39:25 -0400

    added service entity

---
+++ src/main/java/com/waelsworld/backend/models/Service.java
@@ -0,0 +1,33 @@
+package com.waelsworld.backend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "services")
+@Getter
+@Setter
+public class Service extends BaseEntity {
+    @CreatedDate
+    LocalDateTime serviceDate;
+
+    @Enumerated(EnumType.STRING)
+    ServiceStatus status;
+
+    @OneToMany(mappedBy = "service", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<WorkLog> workLogs;
+
+    @NonNull
+    Invoice invoice;
+
+    UUID customerId;
+
+    UUID MechanicId;
+
+}
