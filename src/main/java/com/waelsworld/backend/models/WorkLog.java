2ea73a5d7eed366aae892a7b9ba014e5c030062e
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-19 17:46:33 -0400

    fixed relationship with service and invoice class

--- src/main/java/com/waelsworld/backend/models/WorkLog.java
+++ src/main/java/com/waelsworld/backend/models/WorkLog.java
@@ -1,17 +1,14 @@
 package com.waelsworld.backend.models;
 
 import com.waelsworld.backend.models.enums.WorkLogType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "work_logs")
+@Table(name = "worklogs")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -27,4 +24,12 @@ public class WorkLog extends BaseEntity{
     int quantity;
 
     String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "service_id")
+    Service service;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "invoice_id")
+    Invoice invoice;
 }
