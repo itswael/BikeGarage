3c988ee71bad675e0262b6785e721e2dd481d2fc
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-19 17:45:00 -0400

    removed redundant id field,

--- src/main/java/com/waelsworld/backend/models/Invoice.java
+++ src/main/java/com/waelsworld/backend/models/Invoice.java
@@ -7,24 +7,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "work_logs")
+@Table(name = "invoices")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Invoice extends BaseEntity{
-    @Column(nullable = false)
-    UUID serviceId;
-    @Column(nullable = false)
-    float base;
-    @Column(nullable = false)
-    float tax;
-    @Column(nullable = false)
-    float total;
+    @Column(nullable = false, precision = 10, scale = 2)
+    BigDecimal base;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    BigDecimal tax;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    BigDecimal total;
 
     @OneToMany(mappedBy = "invoice", cascade = CascadeType.ALL, orphanRemoval = true)
     List<WorkLog> workLogs;
