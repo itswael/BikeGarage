20217f30ba73e10cf35f2cea13b0f7e033ffbe49
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-20 22:05:20 -0400

    added service layer between controller and repo

--- src/main/java/com/waelsworld/backend/services/UserService.java
+++ src/main/java/com/waelsworld/backend/services/UserService.java
@@ -1,4 +1,34 @@
 package com.waelsworld.backend.services;
 
+import com.waelsworld.backend.models.User;
+import com.waelsworld.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(UUID id) {
+        userRepository.deleteById(id);
+    }
 }
+
