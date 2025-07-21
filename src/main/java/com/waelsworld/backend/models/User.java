a040f611b759a8f31471bf8df1327131def21bc2
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-20 22:48:13 -0400

    added password field

--- src/main/java/com/waelsworld/backend/models/User.java
+++ src/main/java/com/waelsworld/backend/models/User.java
@@ -19,6 +19,9 @@ public class User extends BaseEntity {
     @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
+    private String password;
+
     @Column(unique = true, nullable = false)
     private String phone;
 
