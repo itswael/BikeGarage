96d91e42e9b285312fd1cfdcbb27db0cd335526f
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-30 23:09:57 -0400

    fixed jwt util to have userDetails object instead of user

--- src/main/java/com/waelsworld/backend/utils/JwtUtil.java
+++ src/main/java/com/waelsworld/backend/utils/JwtUtil.java
@@ -1,11 +1,11 @@
 package com.waelsworld.backend.utils;
 
-import com.waelsworld.backend.models.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -22,9 +22,9 @@ public class JwtUtil {
 
     private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
 
-    public String generateToken(User user) {
+    public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, user.getUsername());
+        return createToken(claims, userDetails.getUsername());
     }
 
     private String createToken(Map<String, Object> claims, String subject) {
@@ -40,7 +40,7 @@ public class JwtUtil {
                 .compact();
     }
 
-    public boolean validateToken(String token, User userDetails) {
+    public boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
