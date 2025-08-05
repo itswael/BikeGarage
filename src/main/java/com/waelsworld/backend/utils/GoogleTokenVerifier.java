67fd6b6d9ec53c66987ad9d521ef8267d0833dfa
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-08-04 22:39:03 -0400

    added an environment variable for the secret token

--- src/main/java/com/waelsworld/backend/utils/GoogleTokenVerifier.java
+++ src/main/java/com/waelsworld/backend/utils/GoogleTokenVerifier.java
@@ -4,18 +4,21 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 
 @Component
 public class GoogleTokenVerifier {
+    @Value("${google.client.id}") // This should match your application.properties or application.yml
+    String GoogleClientId;
 
     public GoogleIdToken.Payload verify(String idTokenString) {
         try {
             GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier
                     .Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance())
-                    .setAudience(Collections.singletonList("YOUR_GOOGLE_CLIENT_ID")) // replace with real client ID
+                    .setAudience(Collections.singletonList(GoogleClientId)) // replace with real client ID
                     .build();
 
             GoogleIdToken idToken = verifier.verify(idTokenString);
