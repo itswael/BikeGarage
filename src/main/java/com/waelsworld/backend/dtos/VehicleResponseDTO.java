a79a66c996b098f2096b0e7af5330bad4046bec5
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-08-10 22:03:04 -0400

    added vehicle response DTO

---
+++ src/main/java/com/waelsworld/backend/dtos/VehicleResponseDTO.java
@@ -0,0 +1,28 @@
+package com.waelsworld.backend.dtos;
+
+import com.waelsworld.backend.models.Service;
+import com.waelsworld.backend.models.User;
+import com.waelsworld.backend.models.enums.VehicleType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VehicleResponseDTO {
+    private UUID id;
+    private String name;
+    private int make;
+    private String registrationNumber;
+    private VehicleType type;
+    private User user;
+    private List<Service> services;
+    private LocalDateTime lastServiceDate;
+}
