687ac9e5ca9aa8b66c6e61c697f644f286e710f0
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-08-14 21:14:52 -0400

    added repository method for getting all vehicles for a user id

--- src/main/java/com/waelsworld/backend/repositories/VehicleRepository.java
+++ src/main/java/com/waelsworld/backend/repositories/VehicleRepository.java
@@ -4,9 +4,16 @@ import com.waelsworld.backend.models.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public class VehicleRepository extends JpaRepository<Vehicle, UUID> {
+    /**
+     * Repository for Vehicle entity
+     * Provides methods to interact with the database
+     */
+    public Optional<List<Vehicle>> findAllByUserId(UUID userId);
 
 }
