127f79fa7c851b48e1eac091c041fc26562a3f6c
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-08-16 20:53:46 -0400

    corrected the access parameter, added a todo to fetch user details directly from the session object if not passed via request body

--- src/main/java/com/waelsworld/backend/services/VehicleService.java
+++ src/main/java/com/waelsworld/backend/services/VehicleService.java
@@ -3,7 +3,9 @@ package com.waelsworld.backend.services;
 import com.waelsworld.backend.dtos.VehicleRequestDTO;
 import com.waelsworld.backend.dtos.VehicleResponseDTO;
 import com.waelsworld.backend.mapper.VehicleMapper;
+import com.waelsworld.backend.models.User;
 import com.waelsworld.backend.models.Vehicle;
+import com.waelsworld.backend.repositories.UserRepository;
 import com.waelsworld.backend.repositories.VehicleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,10 +17,18 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Service
 public class VehicleService {
-    VehicleRepository vehicleRepository;
+    private final VehicleRepository vehicleRepository;
+    private final UserRepository userRepository;
     public VehicleResponseDTO createVehicle(VehicleRequestDTO vehicleDto) {
-        Vehicle vehicle = VehicleMapper.toVehicle(vehicleDto);
+
+        User user = userRepository.findById(vehicleDto.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found with ID: " + vehicleDto.getUserId()));
+        Vehicle vehicle = VehicleMapper.toVehicle(vehicleDto, user);
         Vehicle savedVehicle = vehicleRepository.save(vehicle);
+
+        // TODO: add user details from the token -- if user is adding his vehivle
+        // else fetch fromt he body if the admin is adding a vehicle for a user
+
         return VehicleMapper.from(savedVehicle);
     }
 
