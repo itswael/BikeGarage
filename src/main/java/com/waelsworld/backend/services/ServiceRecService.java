698a305148b65abab2dfdb643af5eec48b53d0bf
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-09-07 23:27:16 -0400

    added a method to create new service

--- src/main/java/com/waelsworld/backend/services/ServiceRecService.java
+++ src/main/java/com/waelsworld/backend/services/ServiceRecService.java
@@ -1,7 +1,24 @@
 package com.waelsworld.backend.services;
 
+import com.waelsworld.backend.dtos.ServiceRequestDTO;
+import com.waelsworld.backend.dtos.ServiceResponseDTO;
+import com.waelsworld.backend.models.ServiceRec;
+import com.waelsworld.backend.repositories.ServiceRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ServiceRecService {
+
+    private final ServiceRepository serviceRepository;
+
+    public ServiceResponseDTO createService(ServiceRequestDTO serviceRequestDTO) {
+        ServiceRec service = new ServiceRec();
+        // Logic to set service properties from serviceRequestDTO
+        service = serviceRepository.save(service);
+        return null;
+    }
+
+
 }
