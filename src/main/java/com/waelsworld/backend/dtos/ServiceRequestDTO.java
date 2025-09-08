5d9b6384986e9871c806958baa24e5f02cd03c6f
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-09-07 23:28:15 -0400

    added service centre id to dto

--- src/main/java/com/waelsworld/backend/dtos/ServiceRequestDTO.java
+++ src/main/java/com/waelsworld/backend/dtos/ServiceRequestDTO.java
@@ -1,7 +1,11 @@
 package com.waelsworld.backend.dtos;
 
+import lombok.Data;
+
 import java.util.UUID;
 
+@Data
 public class ServiceRequestDTO {
     private UUID vehicleId;
+    private UUID serviceCentreId;
 }
