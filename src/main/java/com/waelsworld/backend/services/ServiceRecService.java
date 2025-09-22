9555c5ff4927c1e98c2f6db1b3b49b3437ff5a79
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-09-21 23:47:41 -0400

    fixed a service method to gather the details before creating a new service based on vehicle id

--- src/main/java/com/waelsworld/backend/services/ServiceRecService.java
+++ src/main/java/com/waelsworld/backend/services/ServiceRecService.java
@@ -3,8 +3,11 @@ package com.waelsworld.backend.services;
 import com.waelsworld.backend.dtos.ServiceRequestDTO;
 import com.waelsworld.backend.dtos.ServiceResponseDTO;
 import com.waelsworld.backend.mapper.ServiceMapper;
+import com.waelsworld.backend.mapper.VehicleMapper;
 import com.waelsworld.backend.models.ServiceRec;
+import com.waelsworld.backend.models.Vehicle;
 import com.waelsworld.backend.repositories.ServiceRepository;
+import com.waelsworld.backend.repositories.VehicleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +16,11 @@ import org.springframework.stereotype.Service;
 public class ServiceRecService {
 
     private final ServiceRepository serviceRepository;
+    private final VehicleRepository vehicleRepository;
 
     public ServiceResponseDTO createService(ServiceRequestDTO serviceRequestDTO) {
-        ServiceRec service = new ServiceRec();
+        Vehicle vehicle = vehicleRepository.getById(serviceRequestDTO.getVehicleId());
+        ServiceRec service = ServiceMapper.from(vehicle);
         // Logic to set service properties from serviceRequestDTO
         service = serviceRepository.save(service);
         return ServiceMapper.to(service);
