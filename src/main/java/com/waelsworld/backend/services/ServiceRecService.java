ea811906b7db12a586bdab5ee8bae2b1fbfedf50
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-09-24 21:21:16 -0400

    added service method to get all services for a vehicle id

--- src/main/java/com/waelsworld/backend/services/ServiceRecService.java
+++ src/main/java/com/waelsworld/backend/services/ServiceRecService.java
@@ -3,13 +3,16 @@ package com.waelsworld.backend.services;
 import com.waelsworld.backend.dtos.ServiceRequestDTO;
 import com.waelsworld.backend.dtos.ServiceResponseDTO;
 import com.waelsworld.backend.mapper.ServiceMapper;
-import com.waelsworld.backend.mapper.VehicleMapper;
 import com.waelsworld.backend.models.ServiceRec;
 import com.waelsworld.backend.models.Vehicle;
 import com.waelsworld.backend.repositories.ServiceRepository;
 import com.waelsworld.backend.repositories.VehicleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +27,11 @@ public class ServiceRecService {
         // Logic to set service properties from serviceRequestDTO
         service = serviceRepository.save(service);
         return ServiceMapper.to(service);
+    }
+
+    public List<ServiceResponseDTO> getService(UUID vehicleId) {
+        Optional<ServiceRec> services= serviceRepository.findById(vehicleId);
+        return services.stream().map(ServiceMapper::to).toList();
     }
 
 
